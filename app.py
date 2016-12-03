0f822cfd983494e71e5d9076b29e236fabccbed8
Saurabh Sood <saurabhsood91@gmail.com>
2016-12-03 12:45:21 -0800

    change slash param

--- app.py
+++ app.py
@@ -7,7 +7,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!!!', 200
+    # when the endpoint is registered as a webhook, it must echo back
+    # the 'hub.challenge' value it receives in the query arguments
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
+return "Hello world", 200
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
